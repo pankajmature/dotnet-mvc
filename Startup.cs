70bcfe281e17774c7c6e79fa05fb473394b14679
Pankaj Mature <pankaj.mature@outlook.com>
2021-08-08 18:57:48 +0530

    Commit

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace webmvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +45,7 @@ namespace webmvc
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseMvcWithDefaultRoute();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
